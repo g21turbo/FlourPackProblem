3e70c0c7392f9dfb9d62ce8d2d34ab02cf118b81
g21turbo <dg21stew@gmail.com>
2023-11-09 21:41:56 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+
+
+    public static boolean canPack(int bigCount, int smallCount, int goal) {
+
+        bigCount = bigCount * 5;
+        goal = bigCount + smallCount;
+
+
+
+    }
+
+}
