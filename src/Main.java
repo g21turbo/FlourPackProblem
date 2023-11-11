f31503bbfdbd367b8295105e14a4b1a652737486
g21turbo <dg21stew@gmail.com>
2023-11-11 11:27:13 -0800

    Added canPackOther method, my submission

--- src/Main.java
+++ src/Main.java
@@ -2,35 +2,58 @@ public class Main {
     public static void main(String[] args) {
 
 
-        System.out.println(canPack(1,0,4));
-        System.out.println(canPack(0,5,4));
-        System.out.println(canPack(0,5,4));
-        System.out.println(canPack(2,2,11));
-        System.out.println(canPack(-3,2,12));
+        System.out.println(canPackOther(1,0,4));
+        System.out.println(canPackOther(0,5,4));
+        System.out.println(canPackOther(0,5,4));
+        System.out.println(canPackOther(2,2,11));
+        System.out.println(canPackOther(-3,2,12));
 
     }
 
 
-    public static boolean canPack(int bigCount, int smallCount, int goal) {
 
-        if (bigCount < 0 || smallCount < 0 || goal < 0){
+
+//  ------------------------ Submitted Solution ----------------------------------
+    public static boolean canPackOther(int bigCount, int smallCount, int goal) {
+
+        if (bigCount < 0 || smallCount < 0 || goal < 0) {
             return false;
         }
 
-        bigCount = bigCount * 5;
-        int remainder = bigCount % goal;
+        int totalBigWeight = bigCount * 5;
+        int remainder = goal % 5;
 
-        if ((bigCount + smallCount) % goal == 0) {
+        if (totalBigWeight + smallCount >= goal && smallCount >= remainder) {
             return true;
-        } else if (smallCount > goal){
-            return true;
-        } else if (bigCount % goal < 0 && smallCount < 1){
-            return false;
-        } else if (remainder != 0 && smallCount > remainder){
-            return true;
-        } else if (bigCount == 0 && smallCount < goal){
+        } else {
             return false;
         }
-        return true;
     }
+
+
+
+
+//    --------------------- Previous Attempt --------------------------------
+//    public static boolean canPack(int bigCount, int smallCount, int goal) {
+//
+//        if (bigCount < 0 || smallCount < 0 || goal < 0){
+//            return false;
+//        }
+//
+//        bigCount = bigCount * 5;
+//        int remainder = bigCount % goal;
+//
+//        if ((bigCount + smallCount) % goal == 0) {
+//            return true;
+//        } else if (smallCount > goal){
+//            return true;
+//        } else if (bigCount % goal < 0 && smallCount < 1){
+//            return false;
+//        } else if (remainder != 0 && smallCount > remainder){
+//            return true;
+//        } else if (bigCount == 0 && smallCount < goal){
+//            return false;
+//        }
+//        return true;
+//    }
 }
