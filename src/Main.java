a91e1913e6f7811947ff7c02070e35eb0733125e
g21turbo <dg21stew@gmail.com>
2023-11-11 11:27:55 -0800

    Added canPackNew method, the instructor solution

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,31 @@ public class Main {
     }
 
 
+    // --------------------- Instructors Solution -----------------------------
+    private static final int BIG_WEIGHT = 5;
+
+    public static boolean canPackNew(int bigCount, int smallCount, int goal) {
+
+        if (bigCount < 0 || smallCount < 0 || goal < 0) {
+            return false;
+        }
+
+        boolean result = false;
+        int totalBigWeight = bigCount * BIG_WEIGHT;
+        if (totalBigWeight >= goal) {
+            int remaining = goal % BIG_WEIGHT;
+            if (smallCount >= remaining) {
+                result = true;
+            }
+        } else {
+            if (smallCount >= goal - totalBigWeight) {
+                result = true;
+            }
+        }
+        return result;
+    }
+
+
 
 
 //  ------------------------ Submitted Solution ----------------------------------
